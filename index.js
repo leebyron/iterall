leebyron/iterall

commit d867dbe6f2e365241809f9d3bf5d04d74e9069a7

forEachAwait memory issue (#25)
* Don't nest Promises in forAwaitEach

* Match coding style

index.js
@@ -641,16 +641,20 @@ function forAwaitEach(source, callback, thisArg) {
   var asyncIterator = createAsyncIterator(source)
   if (asyncIterator) {
     var i = 0
-    function next() {
-      return asyncIterator.next().then(function(step) {
-        if (!step.done) {
-          return Promise.resolve(
-            callback.call(thisArg, step.value, i++, source)
-          ).then(next)
-        }
-      })
-    }
-    return next()
+    return new Promise(resolve => {
+      function next() {
+        return asyncIterator.next().then(function(step) {
+          if (!step.done) {
+            Promise.resolve(
+              callback.call(thisArg, step.value, i++, source)
+            ).then(next)
+          } else {
+            resolve()
+          }
+        })
+      }
+      next()
+    })
   }
 }
 exports.forAwaitEach = forAwaitEach

commit 5628ce4b39f62fd31a81f7c7aadbd7dcdbfac54e

Fix build issue introduced in #25
Fixes #26

index.js
@@ -641,7 +641,7 @@ function forAwaitEach(source, callback, thisArg) {
   var asyncIterator = createAsyncIterator(source)
   if (asyncIterator) {
     var i = 0
-    return new Promise(resolve => {
+    return new Promise(function(resolve) {
       function next() {
         return asyncIterator.next().then(function(step) {
           if (!step.done) {